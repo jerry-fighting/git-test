72b705db2a6ad34b853c81061f0cb21d84afd660
tangtang <936298297@qq.com>
2023-01-30 16:34:54 +0800

    hot-fix first commit

--- src/main/java/com/gdufs/git/GitHello.java
+++ src/main/java/com/gdufs/git/GitHello.java
@@ -5,6 +5,6 @@ public class GitHello {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
-        System.out.println("hello git666!");
+        System.out.println("hot-fix test!");
     }
 }
