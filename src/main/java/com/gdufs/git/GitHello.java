4ecf65b756c914126621ad1c778b14fb2829ca24
tangtang <936298297@qq.com>
2023-02-07 16:02:41 +0800

    fourth commit

--- src/main/java/com/gdufs/git/GitHello.java
+++ src/main/java/com/gdufs/git/GitHello.java
@@ -1,5 +1,9 @@
 package com.gdufs.git;
 
+/**
+ * github 的token
+ * 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
+ */
 public class GitHello {
     public static void main(String[] args) {
         System.out.println("hello git!");
@@ -8,5 +12,6 @@ public class GitHello {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test!");
+        System.out.println("ssh test!");
     }
 }
